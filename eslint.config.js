ec151652ee5d8a5b84c1e64ee8d826a200b5d961
Angelo Gabrielli 'tintiz00rd' Tinti <angelo.tinti@gmail.com>
2025-05-06 19:19:06 -0300

    Finished initial setup.

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
+  // Ignorar arquivos e pastas
   { ignores: ['dist', '.eslintrc.cjs'] },
+
+  // Config principal do projeto
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       sourceType: 'module',
       globals: globals.browser,
       parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: new URL('.', import.meta.url),
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommendedTypeChecked[0].rules,
+      ...tseslint.configs.stylisticTypeChecked[0].rules,
+      ...prettier.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-  // Adicionando os configs recomendados
-  {
-    ...js.configs.recommended,
-  },
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      ...prettier.rules, // Desabilita regras que conflitam com Prettier
-    },
-  }
-)
+
+  // Adiciona regras JS padrão do ESLint
+  js.configs.recommended,
+
+  // Aplica configs recomendados do TypeScript separados por motivos técnicos
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/__tests__/setup.ts'],
+  },
+});
